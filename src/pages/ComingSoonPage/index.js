eec7d1dd721a49ab83b0b8e370688043af23b10b
Jur van Toledo <jurvantoledo@live.nl>
2021-12-20 13:04:32 +0100

    added text

--- src/pages/ComingSoonPage/index.js
+++ src/pages/ComingSoonPage/index.js
@@ -22,10 +22,21 @@ export default function ComingSoonPage() {
             <div className="container">
                 <h2>The Secret Garden</h2>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
-                    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    A dense green tunnel. Its undergrowth rustles, its foliage flutters. You continue walking in silence, softly touching leaves with your fingertips. 
+                    A mob of blue tits startles you while they chatter away. They take no heed of you. Small gaps in the thicket reveal bright colors. Flower petals? Butterflies? 
+                    You listen closely. There’s the buzzing of bees, a splash of water, and soft, friendly voices in the distance. You stop. On the path in front of you sits 
+                    a little wren. It points its tail spunkily towards the sky while clenching a spider in its pecker. The bird tilts its head at the sight of you and looks 
+                    you right into the eye. You open your mouth to talk to it, but it has already darted away to the other side of the hedge. You’ll meet again, you’re sure of it.
+                     A breeze, coming from the end of the path, carries the scent of earth, herbs, fruits, and flowers. You follow a bend. A dry stone wall and a gate with a 
+                     round door appear. You step closer: the door is ajar…
+                </p>
+                <p>
+                    The story doesn’t stop here. It only begins. Soon, the door to the Secret Garden will open. Follow the wren and discover grassland, water, hill, 
+                    and hedgerows, but an edible forest and a nursery garden, too. All abound with life, and all mosaic pieces to a unity. You’ll find a great diversity 
+                    of plants, trees, mushrooms, flowers, and wildlife. Still, this place does not just serve itself. You may be part of it. The secret to this garden is 
+                    that it endeavors to meet the needs of both nature and people. It shows how economic value can go hand in hand with the scenic and the natural. Thriving 
+                    flora and fauna don’t have to go at the cost of production. On the contrary, the Secret Garden combines both to its advantage. By doing so, the Garden 
+                    may even strengthen our connection to nature, inspire curiosity, and wonder, and, ultimately, joy. 
                 </p>
             </div>
             <div className="component_2 wrapper" style={{ transform: `translateY(${offsetY * -.75}px)` }}>
