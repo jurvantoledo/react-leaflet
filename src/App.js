8b5a52f36d287b85fc062f37fd6498ad488df8a4
Jur van Toledo <jurvantoledo@live.nl>
2021-12-27 10:30:40 +0100

    added routes component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import './App.css';
-import ComingSoonPage from './pages/ComingSoonPage';
-import { Routes, Route } from "react-router-dom";
-import HomePage from './pages/HomePage';
-import TestPage from './pages/Test';
 import SEO from './Components/SEO';
+import PageRoutes from './PageRoutes';
 
 function App() {  
 
   return (
     <div className="App">
         <SEO />
-        <Routes>
-          <Route exact path='/home' element={<HomePage />} />
-          <Route exact path="/" element={<ComingSoonPage />} />
-          <Route exact path="/test" element={<TestPage />} />            
-        </Routes>
+        <PageRoutes />
     </div>
   );
 }
---
+++ src/PageRoutes/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Routes, Route } from 'react-router'
+import ComingSoonPage from '../pages/ComingSoonPage'
+import HomePage from '../pages/HomePage'
+import TestPage from '../pages/Test'
+
+export default function PageRoutes() {
+    return (
+        <Routes>
+            <Route exact path='/' element={<ComingSoonPage />}/>
+            <Route exact path='/home' element={<HomePage />} />
+            <Route exact path='/test' element={<TestPage />} />
+        </Routes>
+    )
+}
