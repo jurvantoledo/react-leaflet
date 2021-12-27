98da77110f8507297297a84a6e33a27672d3f834
Jur van Toledo <jurvantoledo@live.nl>
2021-12-27 09:45:41 +0100

    added favicon and SEO for favicon

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import ComingSoonPage from './pages/ComingSoonPage';
 import { Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import TestPage from './pages/Test';
+import SEO from './Components/SEO';
 
 function App() {  
 
   return (
     <div className="App">
+        <SEO />
         <Routes>
           <Route exact path='/home' element={<HomePage />} />
           <Route exact path="/" element={<ComingSoonPage />} />
