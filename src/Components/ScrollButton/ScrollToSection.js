4e3f90cf77c57fc60a241f179ce50ccdab4bf25e
Jur van Toledo <jurvantoledo@live.nl>
2022-01-04 12:31:41 +0100

    added pace to function

--- src/Components/ScrollButton/ScrollToSection.js
+++ src/Components/ScrollButton/ScrollToSection.js
@@ -1,5 +1,5 @@
 // Scrolls to the section the ref is called on
-const ScrollToSection = (props) =>{ 
+const ScrollToSection = (props) => { 
         window.scrollTo({ 
             top: props.myRef.current.offsetTop, 
             behavior: 'smooth'
