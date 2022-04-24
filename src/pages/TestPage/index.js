e457a975f6e0b029fb15210145074d335e44c9a7
jurvantoledo <jurvantoledo@live.nl>
2022-04-24 18:21:59 +0200

    made start with wallet connection

--- src/pages/TestPage/index.js
+++ src/pages/TestPage/index.js
@@ -1,40 +1,87 @@
-import Button from "@mui/material/Button";
-import React, { Children, cloneElement, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import video from "../../assets/forest_video.mp4"
-import Section from "../../Layout/Section";
-import SectionWrapper from "../../Layout/Section/SectionWrapper";
-import './style.scss'
+import { Button } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import Web3 from "web3";
 
 export default function TestPage() {
+    const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
+    const [userInfo, setUserInfo] = useState({});
+    const [isConneted, setIsConnected] = useState(false);
+
+    const detectCurrentProvider = () => {
+        let provider;
+        if (window.ethereum) {
+            provider = window.ethereum.request({ method: "eth_requestAccounts" });
+        } else {
+            console.log("Non-Ethereum browser detected. You should consider trying MetaMask!")
+        }
+        return provider;
+    }
+
+    const onConnect = async () => {
+        try {
+            const currentProvider = detectCurrentProvider();
+            console.log(currentProvider);
+            if (currentProvider !== web3.currentProvider) {
+                console.log("Non-Ethereum browser detected. You should consider trying MetaMask!")
+            }
+            // Returns a list of accounts.
+            const userAccount = await web3.eth.getAccounts();
+            // Returns the chain ID of the current connected node
+            const chainId = await web3.eth.getChainId();
+            // First account we found
+            const account = userAccount[0];
+            // Returns the balance of current account
+            let ethBalance = await web3.eth.getBalance(account);
+            saveUserInfo(account, chainId, ethBalance);
+            if (userAccount.length === 0) {
+                console.log('Please connect to Metamask');
+            }
+        } catch (error) {
+            console.log('There was an error fetching your accounts. Make sure your Ethereum client is configured correctly.');
+        }
+    }
+
+    const saveUserInfo = (account, chainId, ethBalance) => {
+        const userAccount = {
+            account: account,
+            connectionId: chainId,
+            balance: ethBalance
+        };
+        // User persisted data
+        window.localStorage.setItem('userAccount', JSON.stringify(userAccount));
+        const userData = JSON.parse(localStorage.getItem('userAccount'));
+        setUserInfo(userData);
+        setIsConnected(true)
+    }
+
+    const onDisconnect = () => {
+        let provider;
+        window.localStorage.removeItem('userAccount');
+        setUserInfo({});
+        setIsConnected(false);
+        // provider = window.ethereum.on('disconnect', provider);
+    }
+    useEffect(() => {
+        function checkConnectedWallet() {
+            const userData = JSON.parse(localStorage.getItem('userAccount'));
+            if (userData !== null) {
+                setUserInfo(userData);
+                setIsConnected(true);
+            }
+        }
+
+        checkConnectedWallet();
+    }, [])
     return (
-        <section className="parallax" style={{
-            height: '100vh',
-            overflow: 'hidden'
-        }}>
-            <div className="parallax-background">
-                <video className='videoTag' autoPlay loop muted>
-                    <source src={video} type="video/mp4"/>
-                </video>
-                <div className="parallax-button">
-                    <Link to={"/"}>
-                        <Button variant="outlined" style={{ color: '#fff', borderColor: '#fff'}}>
-                            Enter the secret garden
-                        </Button>
-                    </Link>
-                </div>
-            </div>
-        </section>
+        <div>
+            {!isConneted ?
+                <Button color="error" variant="contained" onClick={onConnect}>
+                    Connect wallet
+                </Button> :
+                <Button color="success" variant="contained" onClick={onDisconnect}>
+                    Disconnect wallet
+                </Button>
+            }
+        </div>
     )
 }
-
-{/* <>
-<SectionWrapper name='test'>
-    <Section>
-        Dit is section 1
-    </Section>
-    <Section>
-        Dit is section 2
-    </Section>
-</SectionWrapper>
-</> */}
